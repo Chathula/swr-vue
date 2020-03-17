2dfb5171faf239f3883e9d27910b80872c8066b5
chathula <chathula@different.com.au>
2020-03-18 03:01:46 +0530

    updated settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,22 @@
 module.exports = {
   "extends": [
+    "prettier/vue",
     "prettier/@typescript-eslint",
     "plugin:prettier/recommended"
   ],
+  "parserOptions": {
+		"ecmaVersion": 8,
+		"sourceType": "module",
+		"ecmaFeatures": {
+			"impliedStrict": true,
+			"experimentalObjectRestSpread": true
+		},
+		"allowImportExportEverywhere": true
+	},
+  "env": {
+		"es6": true,
+		"browser": true,
+		"node": true,
+		"jest": true
+	},
 }
