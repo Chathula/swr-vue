d41867c176a3dfcac28f716a7dffc84ff635b202
chathula <chathula@different.com.au>
2020-04-23 23:25:16 +0530

    update eslinit config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,9 @@ module.exports = {
   "extends": [
     "prettier/vue",
     "prettier/@typescript-eslint",
-    "plugin:prettier/recommended"
+		"plugin:prettier/recommended",
+		"plugin:@typescript-eslint/eslint-recommended",
+		"plugin:@typescript-eslint/recommended",
   ],
   "parserOptions": {
 		"ecmaVersion": 8,
@@ -19,4 +21,18 @@ module.exports = {
 		"node": true,
 		"jest": true
 	},
+	"rules": {
+		"func-names": [
+			"error",
+			"as-needed"
+		],
+		"no-shadow": "error",
+		"prefer-const": 0,
+		"@typescript-eslint/explicit-function-return-type": 0,
+		"@typescript-eslint/no-use-before-define": 0,
+		"@typescript-eslint/camelcase": 0,
+		"@typescript-eslint/no-var-requires": 0,
+		"@typescript-eslint/no-explicit-any": 0
+	}
 }
+
